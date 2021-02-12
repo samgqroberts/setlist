efb3a7f0731fe2d597bdb3a78209e81f2d27edbc
Sam Roberts <samgqroberts@gmail.com>
2021-02-12 16:31:09 -0500

    plug song data into song list table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
+import data from './song-data.json';
 import styles from './App.module.css';
+import React from 'react';
 
 function App() {
   return (
@@ -10,9 +12,37 @@ function App() {
             <div className={styles.filter}>filter | sort</div>
             <div className={styles.search}>search</div>
           </div>
-          <div className={styles.songListTable}>
+          <table className={styles.songListTable}>
+            <thead>
+              <td>Song Name</td>
+              <td>Original Artist</td>
+              <td>Times</td>
+              <td>Debut</td>
+              <td>Last</td>
+              <td>Gap</td>
+            </thead>
+            <tbody>
+              {data.map(d => (
+                <tr key={d.songName}>
+                  <td>{d.songName}</td>
+                  <td>{d.originalArtist}</td>
+                  {d.aliasOf ? (
+                    <td>Alias of&nbsp;{d.aliasOf}</td>
+                  ) : d.foundInDiscography ? (
+                    <td>Found in Discography</td>
+                  ) : (
+                    <React.Fragment>
+                      <td>{d.playInfo?.times}</td>
+                      <td>{d.playInfo?.debut}</td>
+                      <td>{d.playInfo?.last}</td>
+                      <td>{d.playInfo?.gap}</td>
+                    </React.Fragment>
+                  )}
+                </tr>
+              ))}
+            </tbody>
             Song list table
-          </div>
+          </table>
         </div>
       <div className={styles.roster}>
         Roster
