0aa6136671194267ec3804426fc19c7908dbb5cb
Sam Roberts <samgqroberts@gmail.com>
2021-02-12 15:02:30 -0500

    general layout for setlist choosing scene

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1>Setlist</h1>
+      <div className={styles.content}>
+        <div className={styles.songList}>
+          <div className={styles.header}>
+            <div className={styles.filter}>filter | sort</div>
+            <div className={styles.search}>search</div>
+          </div>
+          <div className={styles.songListTable}>
+            Song list table
+          </div>
+        </div>
+      <div className={styles.roster}>
+        Roster
+      </div>
+      </div>
     </div>
   );
 }
