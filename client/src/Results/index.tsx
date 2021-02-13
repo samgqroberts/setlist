c4e1e1c0403e042facf7af1d6c6541c182d5a631
Sam Roberts <samgqroberts@gmail.com>
2021-02-12 22:09:44 -0500

    handle if roster isn't available in Results

--- client/src/Results/index.tsx
+++ client/src/Results/index.tsx
@@ -3,45 +3,38 @@ import styles from './Results.module.css';
 
 const Results: React.FC<{
   roster: R.Roster | undefined;
-}> = () => {
-  const roster: R.Roster = {
-    s1: '1999',
-    s2: '20-20 Vision',
-    s3: '46 Days',
-    s4: 'About to Run',
-    s5: 'AC/DC Bag',
-    s6: 'Access Me',
-    s7: 'Acoustic Army',
-    s8: 'After Midnight',
-    s9: "Ain't Love Funny"
-  };
+}> = ({ roster }) => {
   return (
     <div className={styles.results}>
       <div className={styles.yourScoreContainer}>
         <div className={styles.banner}>
           <h3>Your score</h3>
         </div>
-        <table className={styles.resultsTable}>
-          <thead>
-            <tr>
-              <th>Selection</th>
-              <th>Result</th>
-              <th># Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            {R.rosterIndices.map((i) => {
-              const slot = R.getIndex(roster, i);
-              return (
-                <tr key={(slot || '') + i}>
-                  <td>{slot || '----'}</td>
-                  <td>Not Played</td>
-                  <td>0</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {roster ? (
+          <table className={styles.resultsTable}>
+            <thead>
+              <tr>
+                <th>Selection</th>
+                <th>Result</th>
+                <th># Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {R.rosterIndices.map((i) => {
+                const slot = R.getIndex(roster, i);
+                return (
+                  <tr key={(slot || '') + i}>
+                    <td>{slot || '----'}</td>
+                    <td>Not Played</td>
+                    <td>0</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          'No roster! (go back and fill one out)'
+        )}
       </div>
       <div className={styles.leaderboardContainer}>
         <div className={styles.banner}>
