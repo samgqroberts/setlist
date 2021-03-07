7ce7b5ae99a8a92cc516abcc688fe1b4785de765
Sam Roberts <samgqroberts@gmail.com>
2021-03-07 09:51:50 -0500

    add sorting to the song list table

--- client/src/ChooseSetlist/SongListTable.tsx
+++ client/src/ChooseSetlist/SongListTable.tsx
@@ -1,24 +1,75 @@
-import { SongDataWithPlayInfo } from 'shared';
+import { useState } from 'react';
+import { assertNever, SongDataWithPlayInfo } from 'shared';
 
 import styles from './SongListTable.module.css';
+
+const columnKeys = ['songName', 'lastPlayed', 'timesPlayed'] as const;
+type ColumnKey = typeof columnKeys[number];
+
+type SortDirection = 'asc' | 'desc';
+
+type Sort = { columnKey: ColumnKey; direction: SortDirection };
+
+const compare = (a: number, b: number) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+};
 
 const SongListTable: React.FC<{
   songs: SongDataWithPlayInfo[];
   onAdd: (songName: string) => void;
 }> = ({ songs, onAdd }) => {
+  const [sort, setSort] = useState<Sort>({
+    columnKey: 'timesPlayed',
+    direction: 'desc'
+  });
+  const onSort = (columnKey: ColumnKey) => {
+    const direction =
+      sort.columnKey === columnKey
+        ? sort.direction === 'asc'
+          ? 'desc'
+          : 'asc'
+        : 'desc';
+    setSort({ columnKey, direction });
+  };
+  const sortedSongs = songs.sort((a, b) => {
+    switch (sort.columnKey) {
+      case 'songName':
+        return sort.direction === 'asc'
+          ? a.songName.localeCompare(b.songName)
+          : b.songName.localeCompare(a.songName);
+      case 'lastPlayed':
+        return sort.direction === 'asc'
+          ? compare(a.playInfo.gap, b.playInfo.gap)
+          : compare(b.playInfo.gap, a.playInfo.gap);
+      case 'timesPlayed':
+        return sort.direction === 'asc'
+          ? compare(a.playInfo.times, b.playInfo.times)
+          : compare(b.playInfo.times, a.playInfo.times);
+      default:
+        assertNever(sort.columnKey);
+    }
+  });
   return (
     <div className={styles.songListTableContainer}>
       <table className={styles.songListTable}>
         <thead>
           <tr>
             <th /> {/* Empty header over 'add' column */}
-            <th>Song Name</th>
-            <th>Last Played</th>
-            <th># Played</th>
+            <th>
+              <button onClick={() => onSort('songName')}>Song Name</button>
+            </th>
+            <th>
+              <button onClick={() => onSort('lastPlayed')}>Last Played</button>
+            </th>
+            <th>
+              <button onClick={() => onSort('timesPlayed')}># Played</button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {songs.map((d) => (
+          {sortedSongs.map((d) => (
             <tr key={d.songName + (d.originalArtist || '')}>
               <td>
                 <button
